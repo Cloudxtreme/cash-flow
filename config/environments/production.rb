f18cbc3acc7216353f6eb308c2d0b4cece23d1b0
Connor Warnock <connorwarnock@gmail.com>
2013-03-15 02:57:53 -0700

    Compiles assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ RailsStripeMembershipSaas::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.precompile += [ 'signup_manifest.js' ]
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
