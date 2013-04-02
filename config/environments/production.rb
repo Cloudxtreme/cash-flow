b395cb38fbe04a7bbae67253020b5e36184c2997
Connor Warnock <connorwarnock@gmail.com>
2013-04-02 02:08:03 -0700

    Changes mailer host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Victorypayment::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'warnockandpolivka.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'victoryframework.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
